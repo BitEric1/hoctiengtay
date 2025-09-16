c21d2b4b3d83b94e9874d50e2e1a80d045653f88
BitEric1 <hoangbit185@gmail.com>
2025-09-16 21:25:44 +0700

    Test lai

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./index.css";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 
@@ -11,6 +11,21 @@ function App() {
                 <h1 className="text-3xl font-bold underline text-center">
                     Hello world!
                 </h1>
+                <h2 className="text-2xl font-bold underline text-center">
+                    Edit <code>src/App.jsx</code> and save to test HMR Edit{" "}
+                    <code>src/App.jsx</code> and save to test HMR
+                </h2>
+                <p className="text-2xl font-black text-center text-blue-600">
+                    Vẫn phải có một file css mới đc :))
+                </p>
+                <div className="card">
+                    {/* <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button> */}
+                </div>
+                <p className="read-the-docs text-center">
+                    Click on the Vite and React logos to learn more
+                </p>
             </div>
         </>
     );
