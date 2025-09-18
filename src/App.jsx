9db27db504f5e0fa1ac4c37a49b0e17def1b68f4
BitEric1 <hoangbit185@gmail.com>
2025-09-18 19:54:20 +0700

    add bootstrap and tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
+import { Button, Form, ListGroup, Modal } from "react-bootstrap";
+
 import "./index.css";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 
 function App() {
-    // const [count, setCount] = useState(0);
+    const [show, setShow] = useState(false);
+    const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false);
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/todos/")
+            .then((res) => res.json())
+            .then((data) => setTasks(data));
+    }, []);
 
     return (
         <>
@@ -20,12 +32,109 @@ function App() {
                 </p>
                 <div className="card">
                     {/* <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button> */}
+                        count is {count}
+                    </button> */}
+                    <p className="read-the-docs text-center">
+                        Click on the Vite and React logos to learn more
+                    </p>
                 </div>
-                <p className="read-the-docs text-center">
-                    Click on the Vite and React logos to learn more
-                </p>
+            </div>
+            <Button
+                variant="primary"
+                onClick={handleShow}
+            >
+                Launch static backdrop modal
+            </Button>
+
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal title</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    I will not close if you click outside me. Do not even try to
+                    press escape key.
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button
+                        variant="secondary"
+                        onClick={handleClose}
+                    >
+                        Close
+                    </Button>
+                    <Button variant="primary">Understood</Button>
+                </Modal.Footer>
+            </Modal>
+            <button
+                type="button"
+                className="btn btn-outline-primary"
+            >
+                Primary
+            </button>
+            <Form>
+                <Form.Group
+                    className="mb-3"
+                    controlId="formBasicEmail"
+                >
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                        type="email"
+                        placeholder="Enter email"
+                    />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group
+                    className="mb-3"
+                    controlId="formBasicPassword"
+                >
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Password"
+                    />
+                </Form.Group>
+                <Form.Group
+                    className="mb-3"
+                    controlId="formBasicCheckbox"
+                >
+                    <Form.Check
+                        type="checkbox"
+                        label="Check me out"
+                    />
+                </Form.Group>
+                <Button
+                    variant="primary"
+                    type="submit"
+                >
+                    Submit
+                </Button>
+            </Form>
+            <div className="pt-4">
+                <ListGroup as="ul">
+                    {tasks.map((task, index) => {
+                        return (
+                            <ListGroup.Item
+                                key={index}
+                                as="li"
+                            >
+                                {task.title}
+                            </ListGroup.Item>
+                        );
+                    })}
+                    <ListGroup.Item
+                        as="li"
+                        active
+                    >
+                        Cras justo odio
+                    </ListGroup.Item>
+                </ListGroup>
             </div>
         </>
     );
